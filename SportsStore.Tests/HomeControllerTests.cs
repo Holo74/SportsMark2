8c1829b8b4387416373d5c808c090a48620dbfbc
Holo74 <njk0074@gmail.com>
2023-06-26 10:52:06 -0700

    Add project files.

---
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using SportsStore.Pages;
+using Xunit;
+namespace SportsStore.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Can_Load_Cart()
+        {
+            // Arrange
+            // - create a mock repository
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Mock<IStoreRepository> mockRepo = new Mock<IStoreRepository>();
+            mockRepo.Setup(m => m.Products).Returns((new Product[] {
+        p1, p2
+    }).AsQueryable<Product>());
+            // - create a cart
+            Cart testCart = new Cart();
+            testCart.AddItem(p1, 2);
+            testCart.AddItem(p2, 1);
+            // Action
+            CartModel cartModel = new CartModel(mockRepo.Object, testCart);
+            cartModel.OnGet("myUrl");
+            //Assert
+            Assert.Equal(2, cartModel.Cart.Lines.Count());
+            Assert.Equal("myUrl", cartModel.ReturnUrl);
+        }
+
+        [Fact]
+        public void Can_Update_Cart()
+        {
+            // Arrange
+            // - create a mock repository
+            Mock<IStoreRepository> mockRepo = new Mock<IStoreRepository>();
+            mockRepo.Setup(m => m.Products).Returns((new Product[] {
+        new Product { ProductID = 1, Name = "P1" }
+    }).AsQueryable<Product>());
+            Cart testCart = new Cart();
+            // Action
+            CartModel cartModel = new CartModel(mockRepo.Object, testCart);
+            cartModel.OnPost(1, "myUrl");
+            //Assert
+            Assert.Single(testCart.Lines);
+            Assert.Equal("P1", testCart.Lines.First().Product.Name);
+            Assert.Equal(1, testCart.Lines.First().Quantity);
+        }
+
+        [Fact]
+        public void Can_Use_Repository()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+        new Product {ProductID = 1, Name = "P1"},
+        new Product {ProductID = 2, Name = "P2"}
+        }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            // Act
+            ProductsListViewModel result =
+                controller.Index(null)?.ViewData.Model as ProductsListViewModel ?? new();
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Paginate()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+        new Product {ProductID = 1, Name = "P1"},
+        new Product {ProductID = 2, Name = "P2"},
+        new Product {ProductID = 3, Name = "P3"},
+        new Product {ProductID = 4, Name = "P4"},
+        new Product {ProductID = 5, Name = "P5"}
+        }).AsQueryable<Product>());
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+            // Act
+            ProductsListViewModel result =
+                controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel ?? new();
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
+        }
+    }
+}
